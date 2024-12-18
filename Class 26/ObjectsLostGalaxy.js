a1362567fb1e0379e160911d2c7e2dc71794a1f4
kaydejay <109537825+kaydejay@users.noreply.github.com>
2024-12-18 15:09:16 -0600

    Create ObjectsLostGalaxy.js

---
+++ Class 26/ObjectsLostGalaxy.js
@@ -0,0 +1,21 @@
+//Create a pizza object that has four properties and three methods.
+
+let pizza = {}
+pizza.shape = 'round'
+pizza.slices = 8
+pizza.toppings = ['spinach', 'cheese', 'olives', 'banana peppers']
+pizza.temperature = 'hot'
+pizza.size = 'large'
+pizza.crust = 'thin'
+pizza.sauce = 'heavy'
+
+
+pizza.estimatedDeliveryTime = function (){
+  console.log('Calculating...')
+}
+pizza.burnMouth = function (){
+  console.log('aSDLKFJIOANLADSKJF')
+}
+pizza.frisbee= function (){
+  console.log('YEEEeeet')
+}
