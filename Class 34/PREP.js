72d1ecc3c90b9cb9ee1758613250cff7072f139f
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-01-26 16:48:26 -0600

    Create PREP.js

---
+++ Class 34/PREP.js
@@ -0,0 +1,15 @@
+//PREP
+//A number 1 to number, if divisible 3 fizz, if 5 buzz, 3&5 fizzbuzz
+//P: num, whole, +
+//R: console
+//E:
+
+function fizzBuzz(num){
+    //loop
+    //conditionals %3&5, %3, %5, 
+    //console.log num or fizz,buzz,fizzbuzz
+}
+
+fizzBuzz(5)// 1,2,fizz,4,buzz
+fizzBuzz(3)// 1,2,fizz
+fizzBuzz(15)// 1,2,fizz,4,buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz
