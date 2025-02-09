2bc7fef428359de5bda7f311535656ff57c26dd7
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-02-09 13:51:58 -0600

    Create asyncAPI.js

---
+++ Class 35/asyncAPI.js
@@ -0,0 +1,6 @@
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(data)
+}
+getACuteDogPhoto()
