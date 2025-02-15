604dc337963c47f2cdc38aa0826cc853bb0391fb
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-02-15 17:23:16 -0600

    Create stopwatch.js

---
+++ JS Crash Course/stopwatch.js
@@ -0,0 +1,18 @@
+//Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+
+stopwatch.color = 'black'
+stopwatch.brand = 'Nike'
+stopwatch.shape = 'round'
+stopwatch.smell = 'new'
+
+stopwatch.start = function (){
+    console.log('Runnnnn')
+}
+stopwatch.stop = function (){
+    console.log('stop running')
+}
+stopwatch.morph = function (){
+    console.log('morphin time!')
+}
