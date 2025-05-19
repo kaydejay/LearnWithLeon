c1c560b33b94b9d6ebcf42243fe93383b5f30982
kaydejay <109537825+kaydejay@users.noreply.github.com>
2025-05-18 20:52:44 -0500

    Update and rename Simple Functions Javascript to Simple Functions Javascript.js

--- Class 14/Simple Functions Javascript.js
+++ Class 14/Simple Functions Javascript.js
@@ -3,7 +3,6 @@
 function addTwoNumbers (num1, num2) {
     alert (num1 + num2)
 }
-
 addTwoNumbers(2,3)
 
 //or const sum = num1 + num2
@@ -14,7 +13,6 @@ addTwoNumbers(2,3)
 function multiplyThreeNumbers (num1, num2, num3){
     consolelog(num1*num2*num3)
 }
-
 multiplyThreeNumbers (1,2,3)
 
 //or, const product = num1 * num2 * num3
@@ -25,5 +23,4 @@ multiplyThreeNumbers (1,2,3)
 function divideTwoNumbers (num1, num2){
     return num1/num2
 }
-
 console.log (dividesTwoNumbers(12,6))
